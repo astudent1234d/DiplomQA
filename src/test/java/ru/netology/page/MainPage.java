a3f02316f5bbc9e1e8048741a8404e3c4ba0ba2f
user <test@>
2021-12-19 20:38:58 +0300

    Update

--- src/test/java/ru/netology/page/MainPage.java
+++ src/test/java/ru/netology/page/MainPage.java
@@ -13,9 +13,6 @@ public class MainPage {
     private SelenideElement buttonBuyByCredit = $(byText("Купить в кредит"));
     private SelenideElement byCard = $(byText("Оплата по карте"));
     private SelenideElement byCredit = $(byText("Кредит по данным карты"));
-//    private SelenideElement buttonBuy = $("[class='button button_size_m button_theme_alfa-on-white']");
-//    private SelenideElement buttonBuyByCredit = $$("[class='button button_view_extra button_size_m button_theme_alfa-on-white']").get(0);
-
 
     public PaymentPage buyWithoutCredit() {
         buttonBuy.click();
